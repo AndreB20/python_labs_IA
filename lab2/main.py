2f04097c0fcee98661d9d8dfdd7e5e8ffc823b07
AndreB20 <andreiburlacu2000@gmail.com>
2022-11-01 19:33:15 +0200

    Lab2 edited

--- lab2/main.py
+++ lab2/main.py
@@ -1,43 +1,45 @@
-from operator import le
-import pyautogui
-import keyboard
-class PianoTiles:
-    def __init__(self):
-        print("Apasa TASTA ESC pentru a inchide programul")
-        x1 = self._mouse_pos('STANGA')[0]
-        while keyboard.is_pressed('enter') : pass
-        x2 = self._mouse_pos('DREAPTA')[0]
-        self.left_x, self.right_x = min (x1,x2), max(x1,x2)
-        self.center_y = pyautogui.size()[1] // 2
-        self.tiles = self._tiles_pos()
-        print("Coordonatele jocului sunt", self.left_x, self.right_x, self.center_y)
- 
-    def _mouse_pos(self, border):
-        print(f'Pune cursorul in {border} marginii ferestrei jocului si apasa ENTER')
-        x,y = 0 , 0
-        while not keyboard.is_pressed('enter') and not keyboard.is_pressed('esc'):
-            x,y= pyautogui.position()
-            position = 'X: ' + str(x).rjust(4) + 'Y:' + str(y).rjust(4)
-            print(position, end='')
-            print('\b' * len(position), end='', flush=True)
-        print(f'{border} border: {x,y}')
-        return x,y
- 
-    def _tiles_pos(self):
-        lenght = self.right_x - self.left_x
-        step= lenght // 4
-        return [(self.left_x + i, self.center_y) for i in range (step//2, lenght, step)]
- 
-    def _is_tile(self,pixel,threshold):
-        color= pyautogui.pixel(*pixel)
-        return True if color[0]<=threshold else False
- 
-    def run(self, *, tile_rgb=10):
-        while not keyboard.is_pressed('esc'):
-            for pos in self.tiles:
-                if self._is_tile(pos, tile_rgb):
-                    pyautogui.click(*pos)
-                    break
- 
-if __name__ == '__main__':
-    PianoTiles().run()
+from hashlib import new
+
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+sender = 'nu_vreau_spam@coneasorin.ro'
+subject = 'Pretul a scazut la'
+to_addr_list = ['andreiburlacu2000@gmail.com']
+cc_addr_list = ['']
+def sendemail(sender,message,subject,to_addr_list,cc_addr_list=[]):
+    try:
+        smtpserver='mail.x-it.ro:26'
+        header = 'From: %s\n' % sender
+        header += 'To: %\n' % ','.join(to_addr_list)
+        header += 'Cc: %\n' % ','.join(cc_addr_list)
+        header += 'Subject: %s\n\n' % subject
+        message = header + message
+
+        server = smtplib.SMTP(smtpserver)
+        server.starttls()
+        server.login(sender, "stiinte216_2022")
+        problems = server.sendemail(sender, to_addr_list, message)
+        server.quit()
+        return True
+    except:
+        print("Error: unable to send email !")
+        return False
+
+def data_scrapping():
+    req = requests.get("https://www.emag.ro/telefon-mobil-apple-iphone-14-pro-max-256gb-5g-deep-purple-mq9x3rx-a/pd/DJDY4LMBM/")
+    soup = BeautifulSoup(req.text, 'html.parser')
+    price = soup.find('p', attrs = {'class':'product-new-price'}).text
+    price = price.replace(".","")
+    price = price.replace(",",".")
+    price = price.replace(" Lei", "")
+    new_price= float(price)
+    if(new_price<7999):
+        sendemail(sender,"Speram sa nu fie teapa",subject,to_addr_list,cc_addr_list=[])
+        print("Avem modificare de pret !")
+    else:
+        print("n avem nica")
+    print(new_price)
+
+data_scrapping()
