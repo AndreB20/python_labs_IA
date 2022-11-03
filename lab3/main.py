7b0415397c63846ce0a3aa81f09ac61b37ee708e
AndreB20 <andreiburlacu2000@gmail.com>
2022-11-04 01:01:49 +0200

    Lab3 rewrite it from the ground and also inspired

--- lab3/main.py
+++ lab3/main.py
@@ -1,43 +1,95 @@
-from operator import le
-import pyautogui
 import keyboard
-class PianoTiles:
-    def __init__(self):
-        print("Apasa TASTA ESC pentru a inchide programul")
-        x1 = self._mouse_pos('STANGA')[0]
-        while keyboard.is_pressed('enter') : pass
-        x2 = self._mouse_pos('DREAPTA')[0]
-        self.left_x, self.right_x = min (x1,x2), max(x1,x2)
-        self.center_y = pyautogui.size()[1] // 2
-        self.tiles = self._tiles_pos()
-        print("Coordonatele jocului sunt", self.left_x, self.right_x, self.center_y)
- 
-    def _mouse_pos(self, border):
-        print(f'Pune cursorul in {border} marginii ferestrei jocului si apasa ENTER')
-        x,y = 0 , 0
-        while not keyboard.is_pressed('enter') and not keyboard.is_pressed('esc'):
-            x,y= pyautogui.position()
-            position = 'X: ' + str(x).rjust(4) + 'Y:' + str(y).rjust(4)
-            print(position, end='')
-            print('\b' * len(position), end='', flush=True)
-        print(f'{border} border: {x,y}')
-        return x,y
- 
-    def _tiles_pos(self):
-        lenght = self.right_x - self.left_x
-        step= lenght // 4
-        return [(self.left_x + i, self.center_y) for i in range (step//2, lenght, step)]
- 
-    def _is_tile(self,pixel,threshold):
-        color= pyautogui.pixel(*pixel)
-        return True if color[0]<=threshold else False
- 
-    def run(self, *, tile_rgb=10):
-        while not keyboard.is_pressed('esc'):
-            for pos in self.tiles:
-                if self._is_tile(pos, tile_rgb):
-                    pyautogui.click(*pos)
-                    break
- 
-if __name__ == '__main__':
-    PianoTiles().run()
+import pyautogui
+import numpy as np
+import cv2
+import time
+
+import win32api, win32con
+
+#So to know how to use this BOT press the "start_button" ('z' in this case) on Top-Left corner and Bottom-Right corner
+#After setting the screen space click manually as fast as possible the "START" tile button, because I didn't set up the
+#blue tile
+
+#Other things: I commented where the code needs to be deleted or commented for a cross-platform.
+
+#REMINDER: import win32api, win32con; def click(x, y)  can be commented and use_win32 = False made it !!
+#also on the tap_tile function delete the click() function because it will be commented or deleted doesn't matter
+
+#And it depends on the site the between_tap_delay it varies, because on some sites it will read the click as double
+
+start_button = "z"
+
+between_tap_delay = 0.2
+
+dark = np.array([0, 0, 0])
+dark_high = np.array([255, 255, 75])
+
+threshold = 10
+
+use_win32 = False
+
+height_multiplier = 1.0
+
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+
+last_lane = None
+
+def tap_tile(image):
+    global coords, WIDTH, HEIGHT, keys, last_lane
+    for temp in range(1, int(HEIGHT * height_multiplier), 5):
+        for i in range(4):
+            if i == last_lane: continue
+
+            last_lane = i
+            x = int(i * WIDTH / 4 + WIDTH / 8)
+            y = HEIGHT - temp
+
+            if image[y][x] > threshold:  # if white aka TRUE [255,255,255]
+                if use_win32:
+                    click(x + coords[0], y + coords[1])
+                else:
+                    pyautogui.click(x=x + coords[0], y=y + coords[1])
+
+                return
+
+#stanga sus
+while True:
+    if keyboard.is_pressed(start_button):
+        mousePos1 = pyautogui.position()
+        print(mousePos1)
+        break
+
+time.sleep(1)
+
+#dreapta jos
+while True:
+    if keyboard.is_pressed(start_button):
+        mousePos2 = pyautogui.position()
+        print(mousePos2)
+        break
+
+WIDTH = mousePos2.x - mousePos1.x
+HEIGHT = mousePos2.y - mousePos1.y
+
+time.sleep(1)
+
+coords = (mousePos1.x, mousePos1.y, mousePos2.x, mousePos2.y)
+
+while True:
+    if keyboard.is_pressed(start_button):   #press again for stopping the game
+        break
+
+    image = np.array(pyautogui.screenshot(region=(mousePos1.x, mousePos1.y, WIDTH, HEIGHT)))
+    image = cv2.inRange(image, dark, dark_high)
+
+    tap_tile(image)
+    time.sleep(between_tap_delay)
+
+    # cv2.imshow("normal", scrot) #for seeing the HSV mode image from a screenshot
+    # cv2.waitKey(0)
